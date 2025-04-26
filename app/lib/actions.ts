ae1af26819d34e20b25a56121b2e47ad17c36f40
Libby Marchetti <lsmarchetti01@gmail.com>
2025-04-26 18:53:36 -0500

    finished chap 13, added error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,12 +18,6 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
-    // const rawFormData = {
-    //     customerId: formData.get('customerId'),
-    //     amount: formData.get('amount'),
-    //     status: formData.get('status'),
-    // }
-
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -33,13 +27,18 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
+    try {
+        await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
+    } catch (error) {
+        console.log(error)
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -53,19 +52,29 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
+    try {
+        await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status= ${status}
         WHERE id = ${id}
     `
-
+    } catch (error) {
+        console.log(error)
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices')
+
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`
+    throw new Error('failed to delete invoice');
+
+    try {
+        await sql`
         DELETE FROM invoices WHERE id = ${id}
     `
-    revalidatePath('/dashboard/invoices')
+        revalidatePath('/dashboard/invoices')
+    } catch (error) {
+        console.log(error)
+    }
 }
